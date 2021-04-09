e1c244281afbfc3f3a81018d3b527d08f5c86b82
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-09 12:29:27 +0900

    [HackerRank] 30DaysOfCode - day 0

---
+++ hackerRank/Tutorial/30 Days of Code/day-0.js
@@ -0,0 +1,23 @@
+/**
+ * @see https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+ * @description
+ * # Title: Hello, World
+ * # Condition
+ * 1. stdin에서 입력을 받아와 변수에 저장
+ *
+ * # Problem
+ *  'Hello, World.'를  첫 줄에 출력, 변수의 값을 두번 째 줄에 출력
+ *
+ * # Input
+ * 문자열 inputString
+ * @arguments
+ * 'Welcome to 30 Days of Code!'
+ */
+
+const inputString = 'Welcome to 30 Days of Code!';
+processData(inputString);
+
+function processData(inputString) {
+  console.log('Hello, World.');
+  console.log(inputString);
+}

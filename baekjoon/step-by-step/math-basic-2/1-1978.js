16cb45490b6dfabc4c05445cbd92ebb0b57ab374
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-17 17:52:18 +0900

    [Baekjoon] 기본수학2/1978 - 소수 찾기

---
+++ baekjoon/step-by-step/math-basic-2/1-1978.js
@@ -0,0 +1,46 @@
+/**
+ * @see https://www.acmicpc.net/problem/1978
+ * @description
+ * # Title: 소수 찾기
+ * # Condition
+ * 1.
+ * # Problem
+ * 주어진 수 N개 중에서 소수의 개수를 출력
+ * # Input
+ * 첫 줄에 수의 개수 N (N < 100)
+ * 둘째 줄에 N개의 1000 이하 자연수
+ * # Memo
+ * 소수: 1보다 큰 자연수 중 1과 자기 자신만을 약수로 가지는 수
+ * if (x > 1)
+ * for (let i = 2; i < x; i++)
+ * x % i === 0
+ * @see
+ * @arguments
+ * `4
+    1 3 5 7`
+ */
+
+function countDecimal(nums) {
+  const count = nums.reduce((count, numString) => {
+    const num = Number(numString);
+    if (num <= 1) return count;
+
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) return count;
+    }
+
+    return count + 1;
+  }, 0);
+
+  return count;
+}
+
+const sample = `4
+1 3 5 7`;
+
+const input = sample.split('\n');
+const nums = input[1].split(' ');
+
+const decimalCount = countDecimal(nums);
+
+console.log(decimalCount);
